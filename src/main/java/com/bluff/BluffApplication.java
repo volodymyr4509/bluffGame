362768794b5122ebda7501c8dfcf827b89a850b8
vkret <vkret@edunav.com>
2018-09-18 12:56:14 +0300

    initial commit

---
+++ src/main/java/com/bluff/BluffApplication.java
@@ -0,0 +1,12 @@
+package com.bluff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluffApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bluff/bluff/BluffApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bluff.bluff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BluffApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
